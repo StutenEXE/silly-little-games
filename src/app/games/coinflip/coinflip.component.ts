8ffaa8468c0ef233bffea6b4e95e98dcf406e9ea
Alexandre <alexandre.bidaux78@gmail.com>
2023-03-28 13:06:40 +0200

    Coinflip progress

--- src/app/games/coinflip/coinflip.component.ts
+++ src/app/games/coinflip/coinflip.component.ts
@@ -1,6 +1,9 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { DashboardComponent } from 'src/app/dashboard/dashboard.component';
+import { Game } from 'src/app/shared/models/game';
+import { GameService } from 'src/app/shared/services/games.service';
 import { UserService } from 'src/app/shared/services/user.service';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-coinflip',
@@ -9,18 +12,63 @@ import { UserService } from 'src/app/shared/services/user.service';
 })
 export class CoinflipComponent implements OnInit {
 
-  constructor(private userService: UserService, private dashboard: DashboardComponent) { }
+  readonly GAME_NAME = "Coinflip";
+
+  coinFlipGame!: Game;
+  error: string = '';
+  bet: number = 2;
+
+  constructor(private userService: UserService,
+    private gameService: GameService,
+    protected dashboard: DashboardComponent) { }
 
   ngOnInit(): void {
+    this.gameService.getGame(this.GAME_NAME).subscribe({
+      next: (game) => {
+        if (game === undefined) {
+          this.error = "Game not found";
+        }
+        else {
+          this.coinFlipGame = game;
+        }
+      }
+    })
   }
 
-
   playGame() {
-    this.dashboard.user.coins -= 2;
-    if(Math.random() < 0.5) {
-      this.dashboard.user.coins += 4;
+    if (this.betIsInvalid()) {
+      return;
+    }
+    this.error = "";
+    this.dashboard.user.coins -= this.bet;
+    if (Math.random() < 0.5) {
+      this.dashboard.user.coins += this.bet * 2;
     }
     this.userService.updateCoins(this.dashboard.user.coins);
   }
 
+  setBetButton(bet: number, event: Event) {
+    this.bet = bet;
+  }
+
+  setBetInput(event: any) {
+    this.bet = event.target.value;
+  }
+
+  betIsInvalid() {
+    if (this.dashboard.user === undefined || this.dashboard.user === null) {
+      this.error = "Wait for game to load";
+      return true;
+    }
+    if (this.bet < this.coinFlipGame.bets.values[0]) {
+      this.error = "The minimum bet is " + this.coinFlipGame.bets.values[0];
+      return true;
+    } 
+    if(this.bet > this.dashboard.user.coins) {
+      this.error = "You can't bet what you don't have";
+      return true;
+    }
+    return false;
+  }
 }
+
--- src/app/games/gamelist/gamelist.component.ts
+++ src/app/games/gamelist/gamelist.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Game } from 'src/app/shared/models/game';
+import { GameService } from 'src/app/shared/services/games.service';
 
 @Component({
   selector: 'app-gamelist',
@@ -7,9 +9,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GamelistComponent implements OnInit {
 
-  constructor() { }
+  games!: Game[];
+
+  constructor(private gameService: GameService) { }
 
   ngOnInit(): void {
+    this.gameService.getGames().subscribe({
+      next: (games) => {
+        this.games = games;
+      }
+    }); 
   }
 
 }
