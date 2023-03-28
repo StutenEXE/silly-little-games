1d54d37cba0bd4b575fb411ddcc4642f8d4c48e5
Alexandre <alexandre.bidaux78@gmail.com>
2023-03-28 22:56:51 +0200

    No more onlyNumber directive

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,15 +1,12 @@
 import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
 import { LoaderModule } from "./components/loader/loader.module";
-import { OnlyNumber } from "./directives/onlyNumber.directive";
 
 @NgModule({
     imports: [
         CommonModule
     ],
-    declarations: [
-        OnlyNumber,
-    ],
+    declarations: [ ],
     exports: [
         LoaderModule
     ]
